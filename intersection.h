e099dd740f3adedb484ff800d3e7e35c82043d0a
vasnaz <vas.naz@csu.fullerton.edu>
2018-03-17 22:42:02 -0700

    Skeleton Code
    Skeleton Code to start making the process (along with the outline of the process and classes)

---
+++ intersection.h
@@ -0,0 +1,28 @@
+//quadrant class
+
+
+struct quadrant {
+
+    //lock current state
+    //lock next state
+
+
+};
+
+
+template <typename E>
+
+class intersection {
+
+public:
+
+//functions to lock areas of intersection
+
+
+private:
+
+quadrant *Q;
+
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,30 @@
+/* 351-04 Concurrency Project
+Project Members: Bill Nazaroff, <insert names> */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+void main() {
+
+
+//TO DO
+//Create Intersection of 5 quadrants
+//Create random child processes (cars)
+//Create while loop
+    /*Loop needs to take in cars, check the path in the intersection, and check locks */
+
+//Process
+//1. Enqueue cars into Linked list and add to array (if necessary)
+//2. If any cars have gone (i.e. driven their route), move to next quadrant
+//2a. Release Current quad lock and next state quad lock
+//2b. Lock current quad lock of the next quadrant of the route
+//2c. If car is finished with route, release lock and dequeue
+//3. Next Car: Lock current quadrant (for clock cycle 1) and lock next state
+//4. Look for other cars that can lock current and next state 
+
+
+}
