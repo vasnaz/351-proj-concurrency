e33ab5b149aa893cf3df2c633b4daf80a49345e7
jonathanbedoy <jonathanbedoy1@gmail.com>
2018-03-20 00:41:42 -0600

    Added Pthreads.

--- intersection.h
+++ intersection.h
@@ -1,28 +1,68 @@
 //quadrant class
 //Jonathan and Beau
 
+
+#include <iostream>
+#include <vector>
+#include <semaphore.h>
+using namespace std;
+//sem_t lockQSem;
+sem_t readA;
 struct quadrant {
 
     //current state lock 
     // next state lock
-
-
+	int num;
+	bool full;
 };
 
 
-template <typename E>
+//template <typename E>
 
 class intersection {
-
-public:
-
-//functions to lock areas of intersection
-
-
 private:
 
-quadrant *Q;
+	vector<quadrant> Q;
+
+public:
+	intersection()
+	{
+		//initiallize the semaphore. Might move this onto main if necessary.
+		//make 4 quadrants that are numbered 1-4 and set their states to not full
+		for(int i = 0; i < 4; i++)
+		{
+			quadrant p;
+			p.num = i+1;
+			p.full = false;
+			Q.push_back(p);
+		}
+	}
+//functions to lock areas of intersection
+	bool access(int quad)
+	{
+		sem_wait(&readA);
+		if(!Q[quad-1].full)
+		{
+			Q[quad-1].full = true;
+			cout << "Quadrant " << Q[quad-1].num << " available and snatched.\n";
+			sem_post(&readA);
+			return true;
+		}
+		else
+		{
+			cout << "Quadrant " << Q[quad-1].num << " unavailable.\n";
+			sem_post(&readA);
+			return false;
+		}
+	}
+	void setFree(int quad)
+	{
+		Q[quad-1].full = false;
+		cout << "Quadrant " << Q[quad-1].num << " is free.\n";
+	}
 
 
 
-}
+
+
+};
--- main.cpp
+++ main.cpp
@@ -1,37 +1,48 @@
-/* 351-04 Concurrency Project
-Project Members: Bill Nazaroff, <insert names> */
-
-
 #include <iostream>
-#include <vector>
-#include <string>
+#include <pthread.h>
+#include <semaphore.h>
+#include <cstdlib>
+#include <stdlib.h>
+#include <unistd.h>
+#include "intersection.h"
 
-using namespace std;
+#define NUM_THREADS 4
 
-
-void main() {
-
-
-
-
-
-//TO DO: Joon and Bill
-//Create Intersection of 5 quadrants
-//Create random child processes (cars)
-//Create while loop
-    /*Loop needs to take in cars, check the path in the intersection, and check locks */
-
-//Process
-//1. Enqueue cars into Linked list and add to array (if necessary)
-//2. If any cars have gone (i.e. driven their route), move to next quadrant
-//2a. Release Current quad lock and next state quad lock
-//2b. Lock current quad lock of the next quadrant of the route
-//2c. If car is finished with route, release lock and dequeue
-//3. Next Car: Lock current quadrant (for clock cycle 1) and lock next state
-//4. Look for other cars that can lock current and next state 
-
-
-
-
-
+intersection a;
+void *PrintHello(void *threadid)
+{
+	long tid;
+	tid = (long)threadid;
+	for(int i = 0; i< 4; i++)
+	{
+	sleep(rand()%4);
+	
+	a.access(i);
+	cout << "Thread: " << tid << endl;
+	
+	}
+	pthread_exit(NULL);
+}
+int main()
+{
+	pthread_t threads[NUM_THREADS];
+	int rc;
+	int i;
+	sem_init(&readA, 0, 1);
+	
+	for(i = 0; i < NUM_THREADS; i++)
+	{
+		cout << "main (): creating thread: "<< i << endl;
+		rc = pthread_create(&threads[i], NULL, PrintHello, (void*) i);
+		if(rc)
+		{
+			cout << "unable to create thread: "<< i << endl;
+			exit(-1);
+		}
+	}
+	pthread_exit(NULL);
+	
+	sem_destroy(&readA);
+	
+	return 0;
 }
