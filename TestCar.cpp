cda0f220ed431730fc973791f0a82fc7e02fdbb0
Brian Bugert <coolbots7@gmail.com>
2018-03-19 18:34:55 -0700

    Create program to test car class

---
+++ TestCar.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include "car.h"
+
+int main() {
+	int Straight = 0;
+	int Left = 0;
+	int Right = 0;
+	int UTurn = 0;
+	
+	//Create new car object
+	//Car car;
+	
+	//Create 10 cars
+	for(int i=0;i<100;i++) {
+		Car car;
+		
+		switch(car.GetDesiredDirection()) {
+			case STRAIGHT:
+				Straight++;
+				break;
+			case LEFT:
+				Left++;
+				break;
+			case RIGHT:
+				Right++;
+				break;
+			case UTURN:
+				UTurn++;
+				break;
+		}
+	}
+	
+	printf("Straight: %i\n", Straight);
+	printf("Left: %i\n", Left);
+	printf("Right: %i\n", Right);
+	printf("UTurn: %i\n", UTurn);
+	
+	
+}
